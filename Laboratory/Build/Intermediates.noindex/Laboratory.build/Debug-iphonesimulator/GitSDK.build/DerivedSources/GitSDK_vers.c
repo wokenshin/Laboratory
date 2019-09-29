1a49320a02de33a7ece7043c3877e2d3f6b0cfa2
kenshin <wokenshin@vip.qq.com>
2019-09-29 19:41:46 +0800

    辣鸡

---
+++ Laboratory/Build/Intermediates.noindex/Laboratory.build/Debug-iphonesimulator/GitSDK.build/DerivedSources/GitSDK_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char GitSDKVersionString[];
+ extern const double GitSDKVersionNumber;
+
+ const unsigned char GitSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GitSDK  PROJECT:Laboratory-1" "\n";
+ const double GitSDKVersionNumber __attribute__ ((used)) = (double)1.;
