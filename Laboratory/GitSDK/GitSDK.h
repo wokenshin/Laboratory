4ed8b392ac91c7e0ee26ccf690e1128cb8353786
kenshin <wokenshin@vip.qq.com>
2019-09-29 19:36:37 +0800

    初始化SDK 用来测试 看看如何将SDK合并到另外一个分支

---
+++ Laboratory/GitSDK/GitSDK.h
@@ -0,0 +1,19 @@
+//
+//  GitSDK.h
+//  GitSDK
+//
+//  Created by kenshin van on 2019/9/29.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GitSDK.
+FOUNDATION_EXPORT double GitSDKVersionNumber;
+
+//! Project version string for GitSDK.
+FOUNDATION_EXPORT const unsigned char GitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitSDK/PublicHeader.h>
+
+
