8fd17e6b6b22f7343fefa4b9e2bf50fe95d277c6
v_xiwangfan <v_xiwangfan@tencent.com>
2019-10-17 17:36:10 +0800

    first push

---
+++ Laboratory/Laboratory/Classes/Other/FirstPush.h
@@ -0,0 +1,13 @@
+//
+//  FirstPush.h
+//  Laboratory
+//
+//  Created by leeguo-xs on 2019/10/17.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#ifndef FirstPush_h
+#define FirstPush_h
+
+
+#endif /* FirstPush_h */
